a655f2ef927d0a47ec0123352c18fa6d5f7495e9
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-23 17:49:35 +0300

    Add files via upload

---
+++ docs/html/dir_dc3c72b41a96d4ccd38577c9a7e9944b.js
@@ -0,0 +1,4 @@
+var dir_dc3c72b41a96d4ccd38577c9a7e9944b =
+[
+    [ "main.cpp", "main_8cpp.html", "main_8cpp" ]
+];
---
+++ docs/html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readcoefficient_0',['readCoefficient',['../main_8cpp.html#aa7f96d788c83744d581c890bf910c8d2',1,'main.cpp']]]
+];
