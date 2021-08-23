6d604884eb7cee3a47434e7da4e534b9d18bba7e
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-23 16:48:40 +0300

    Delete doc directory

--- doc/html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['isequalzero_0',['isEqualZero',['../main_8cpp.html#af1f2ee5a40c47d52500b39dc5c2072f9',1,'main.cpp']]],
-  ['islesszero_1',['isLessZero',['../main_8cpp.html#a287c0a04d76ddbb35f89c71d7a11a830',1,'main.cpp']]]
-];
