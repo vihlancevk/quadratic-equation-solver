22ad85c806474c53224bef88f0ecdbf73bd719b2
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-23 16:34:34 +0300

    Delete html directory

--- html/dir_978cd44a092f22344635eff21fd2982b.js
+++
@@ -1,4 +0,0 @@
-var dir_978cd44a092f22344635eff21fd2982b =
-[
-    [ "quadraticEquation1", "dir_dc3c72b41a96d4ccd38577c9a7e9944b.html", "dir_dc3c72b41a96d4ccd38577c9a7e9944b" ]
-];
--- html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['clearinputbuffer_0',['clearInputBuffer',['../main_8cpp.html#a22d7c3e857afba4903a76181f91ea9fb',1,'main.cpp']]]
-];
