b41f1b5d9f859c212f67e196cc9c02c69e606c81
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-24 17:24:27 +0300

    Add files via upload

---
+++ docs/search/functions_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['solvelinearequation_0',['solveLinearEquation',['../main_8cpp.html#abe80d8a98290766b6625b5b16b171b03',1,'main.cpp']]],
+  ['solvequadraticequation_1',['solveQuadraticEquation',['../main_8cpp.html#aa402b2c35eaa0700c55516419027458a',1,'main.cpp']]]
+];
