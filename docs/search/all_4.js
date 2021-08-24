6733ec672057dd442fa478c207ad5efa474ff6b5
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-24 14:07:52 +0300

    Delete docs directory

--- docs/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['precision_0',['PRECISION',['../main_8cpp.html#ab0fe7f331f3e51db32d0a37539dc8fba',1,'main.cpp']]],
-  ['processcommandlinearguments_1',['processCommandLineArguments',['../main_8cpp.html#a5968ba4cd18cbd5856751ed572b7d52c',1,'main.cpp']]]
-];
