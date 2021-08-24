917e41e0c75ec75014d860e685dfaebce675fbb4
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-24 21:59:01 +0300

    Delete docs directory

--- docs/files_dup.js
+++
@@ -1,4 +0,0 @@
-var files_dup =
-[
-    [ "main.cpp", "main_8cpp.html", "main_8cpp" ]
-];
--- docs/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['processcommandlinearguments_0',['processCommandLineArguments',['../main_8cpp.html#a5968ba4cd18cbd5856751ed572b7d52c',1,'main.cpp']]]
-];
