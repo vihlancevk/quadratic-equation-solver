5fe92831317fa3e499a9051f47f0474e64c68242
vihlancevk <44124531+vihlancevk@users.noreply.github.com>
2021-08-25 20:37:53 +0300

    Add files via upload

---
+++ src/testProgram.h
@@ -0,0 +1,11 @@
+#ifndef TESTPROGRAM_H_
+#define TESTPROGRAM_H_
+
+void testCompareZero(float number, int correctAnswer);
+void testSolveLinearEquation(float a, float b, float correctX, int correctRootsCount);
+void testSolveQuadraticEquation(float a, float b, float c, float correctX1,
+                                float correctX2, int correctRootsCount);
+void processCommandLineArguments(const int argc, char const *argv[]);
+void testProgram();
+
+#endif
